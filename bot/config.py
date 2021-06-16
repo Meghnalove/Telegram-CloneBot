9d932cf36179e9dcbedeae621020ff3bebb0ba19
Meghnalove <85968015+Meghnalove@users.noreply.github.com>
2021-06-16 13:28:23 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,12 +3,12 @@ import json
 from distutils.util import strtobool as stb
 
 # --------------------------------------
-BOT_TOKEN = ""
-GDRIVE_FOLDER_ID = ""
+BOT_TOKEN = "1721730854:AAFNNLlJtbY8F8Hy_K1YT5Ozpn4fYe_mw1Q"
+GDRIVE_FOLDER_ID = "https://drive.google.com/folderview?id=1KA28OavCxOhNBR_tcbJRR81OWURM0FxB"
 # Default folder id.
-OWNER_ID = 123455673
+OWNER_ID = 930652019
 # Example: OWNER_ID = 619418070
-AUTHORISED_USERS = []
+AUTHORISED_USERS = [930652019]
 # Example: AUTHORISED_USERS = [63055333, 100483029, -1003943959]
 INDEX_URL = ""
 IS_TEAM_DRIVE = True
